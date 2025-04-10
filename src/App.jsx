3e03233957dd4a3cdb54dab5db576f58b975e77d
KOLEN94 <breaknius@gmail.com>
2025-04-10 14:55:00 -0500

    actualizar cv

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './page/Home';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -52,7 +52,7 @@ export default function Hero() {
               <Button variant="outline-light"
                 className="titulo-nav-menu boton-cv"
                 rel="noreferrer"
-                href="https://drive.google.com/file/d/1BzbLrSteaABnCfxvtRSuI-KXCyJ4xUwo/view?usp=sharing"
+                href="https://drive.google.com/file/d/1ov1beb9zntZLWe2AndoFKh1Yt4Q4BFq8/view?usp=sharing"
                 target="_blank">
                 <AiOutlineFilePdf /> Descargar cv
               </Button>
